6c90d635991689d92def18a152860800b27f3d27
Mannick <manuel.antonio.c2018@gmail.com>
2020-05-02 12:16:31 -0400

    commit rama

---
+++ for_02.py
@@ -0,0 +1,50 @@
+numero = 0;
+
+
+while numero != 2:
+    miEmail = input("Ingrese su mail: ")
+    numero_arroba = 0
+    numero_punto = 0
+    for arrobas in miEmail:
+        if arrobas == "@":
+            numero_arroba = numero_arroba + 1
+    for puntos in miEmail:
+        if puntos == ".":
+            numero_punto = numero_punto + 1     
+    numero = numero_arroba + numero_punto
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+    
+
+
+# for i in miEmail:
+#     if(i == "@" or i == ".")
+#         contador = contador + 1
+
+# if contador == 2:
+#     print("email bueno")
+# else:
+#     print("El email no es correcto")
+
+
+    
+
