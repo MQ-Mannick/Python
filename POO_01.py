a3f4d6f5c5098a7d7228cf004bc7c473fa76341d
Mannick <manuel.antonio.c2018@gmail.com>
2020-05-11 11:32:05 -0400

    11-5-2020

--- POO_01.py
+++ POO_01.py
@@ -1,21 +1,59 @@
 class Coche():
     #PROPIEDADES
-    largoChasis = 250
-    anchoChasis = 120
-    ruedas = 4
-    en_marcha = False
+    #ESTADO INICIAL-------------------------------------
+    def __init__(self):
+        self.__largoChasis = 250   
+        self.__anchoChasis = 120   
+        self.__ruedas = 4           
+        self.__en_marcha = False
+    #---------------------------------------------------
 
     #COMPORTAMIENTO
-    def arrancar(self): #SELF palabra reservada para definir metodos.
-        self.en_marcha = True
-    def estado(self):
-        if (self.en_marcha) == True:
+    def arrancar(self, arrancamos): #SELF palabra reservada para definir metodos.
+        self.__en_marcha = arrancamos
+
+        if(self.__en_marcha) == True:
+            chequeo = self.__chequeo_interno()
+
+        if (self.__en_marcha and chequeo) == True:
             return "El coche está en marcha"
+
+        elif(self.__en_marcha == True and chequeo == False):
+            return "algo ha ido mal en el chequeo"
+
         else:
             return "El coche esta en pana"
 
-miCoche = Coche() #INSTANCIAR LA CLASE, SE CREA UN OBJ
 
-miCoche.arrancar()
-print(miCoche.estado())
+    def estado(self):
+        print("el coche tiene", self.__ruedas, "ruedas")
+        print("el coche tiene un ancho de", self.__anchoChasis)
+        print("el coche tiene un largo de", self.__largoChasis)
+
+
+
+    def __chequeo_interno(self): #Metodo encapsulado
+        print("realizando chequeo interno")
+
+        self.gasolina = "ok"
+        self.aceite = "ok"
+        self.puertas = "cerradas"
+
+        if self.gasolina == "ok" and self.aceite == "ok" and self.puertas == "cerradas":
+            return True
+        else:
+            return False
+     
+
+
+
+miCoche = Coche() #INSTANCIAR LA CLASE, SE CREA UN OBJ
+print(miCoche.arrancar(True))
+miCoche.estado()
+
+print("------------------------------------------------")
+
+miCoche_2 = Coche() #INSTANCIA SECUNDO OBJETO
+print(miCoche_2.arrancar(False))
+miCoche_2.estado()
 
