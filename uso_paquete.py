ea50f739ac9fddf9619bfdb26758918ef4894397
Mannick <manuel.antonio.c2018@gmail.com>
2020-05-19 11:35:17 -0400

    19-5-2020

--- uso_paquete.py
+++ uso_paquete.py
@@ -1,7 +1,5 @@
-from Paquete_calculos.mul_div.calculo_mul_div import multiplicar, dividir
+
 from Paquete_calculos.suma_resta.calculo_suma_resta import sumar, restar
 
 sumar(10,5)
 restar(10,5)
-multiplicar(10,5)
-dividir(10,5)
