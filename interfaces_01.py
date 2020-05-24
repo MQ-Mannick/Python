43175172f6b58e7a5920ca17a36075ddfb960b2d
Mannick <manuel.antonio.c2018@gmail.com>
2020-05-24 10:49:02 -0400

    24-5-2020

---
+++ interfaces_01.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+raiz = Tk()
+
+raiz.title("My first window title ctm")
+
+raiz.resizable(True,True)
+
+raiz.iconbitmap("favicon.ico")
+
+raiz.geometry("1920x1080")
+
+raiz.config(bg = "black")
+
+raiz.mainloop()
